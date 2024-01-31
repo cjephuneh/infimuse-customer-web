e74a3fcb4c39076f3213cf9b44ebce29ddcb99cc
Teeticode <teetinormm@gmail.com>
2024-01-31 23:19:30 +0300

    UI:add floating card on class details page

--- app/packages/[packageId]/page.jsx
+++ app/packages/[packageId]/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaRegStar } from "react-icons/fa";
+import { FaRegStar, FaLongArrowAltRight } from "react-icons/fa";
 
 const PackageDetails = () => {
   return (
@@ -21,8 +21,23 @@ const PackageDetails = () => {
           </div>
         </div>
       </div>
-
-      <div>floating card</div>
+      {/* floating card */}
+      <div className="w-[300px] rounded-xl absolute top-10 right-14 bg-white p-5 shadow-lg">
+        <p className="font-light">class price</p>
+        <h1 className="text-2xl font-bold">Ksh 10,000</h1>
+        <p>Course format</p>
+        <ul>
+          <li>5 video lessons</li>
+          <li>4 quizes for testing your knowledge</li>
+          <li>3 assignments for you to practice</li>
+          <li> 1 project to showcase your skills</li>
+          <li>course certificate</li>
+        </ul>
+        <button className="flex items-center space-x-2  bg-green-500 text-white py-2 px-5 rounded-3xl">
+          <p>Enroll now</p>
+          <FaLongArrowAltRight />
+        </button>
+      </div>
       <div>details about class</div>
     </div>
   );
