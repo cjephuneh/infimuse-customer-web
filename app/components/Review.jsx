74b04eb312734610555b6a22b41f6e73eb4ab1f9
Teeticode <teetinormm@gmail.com>
2024-02-02 11:45:44 +0300

    UI: progrees on reviews tab

---
+++ app/components/Review.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import React from "react";
+import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa6";
+
+const Review = () => {
+  return (
+    <div className="grid grid-cols-3 gap-2 mt-3">
+      <div className="flex items-center col-span-1 gap-3">
+        <Image
+          className="w-12 h-12 rounded-full"
+          src={"/image_7.jpg"}
+          alt="Reviewer Image"
+          width={250}
+          height={150}
+        />
+        <div className="">
+          <h2 className="font-semibold">Jane Doe</h2>
+          <small className="text-gray-400">8 months ago</small>
+          <div className="flex space-x-">
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaRegStar className="text-yellow-500" />
+          </div>
+        </div>
+      </div>
+      <div className="flex col-span-2">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quae
+          qui eligendi nobis quos, nam accusamus similique excepturi laboriosam
+          totam?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
--- app/components/Reviews.jsx
+++ app/components/Reviews.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Review from "./Review";
 
 const Reviews = () => {
   return (
-    <div>
-      <h1>this is the reviews section</h1>
+    <div className="w-7/12 space-y-5">
+      <h1 className="text-xl font-bold">Reviews (23)</h1>
+      <hr className="h-0.5 bg-green-500" />
+      <Review />
+      <hr className="h-0.5 bg-green-500" />
+      <Review />
+      <hr className="h-0.5 bg-green-500" />
+      <Review />
     </div>
   );
 };
